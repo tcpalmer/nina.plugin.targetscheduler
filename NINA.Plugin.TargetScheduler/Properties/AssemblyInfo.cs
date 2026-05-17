bbbd1413de74e43753abd5210e10dfc8bc8d1b7e
Tom Palmer <palmer.tom@gmail.com>
2026-05-17 14:39:19 -0400

    updated nina version to 3.3 1037

--- NINA.Plugin.TargetScheduler/Properties/AssemblyInfo.cs
+++ NINA.Plugin.TargetScheduler/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("5.10.1.0")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.3.0.1035")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.3.0.1037")]
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
 [assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/en-US/MPL/2.0/")]
 [assembly: AssemblyMetadata("Repository", "https://github.com/tcpalmer/nina.plugin.targetscheduler/")]
